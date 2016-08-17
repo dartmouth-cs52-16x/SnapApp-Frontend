25ceafa568798334962002960eed0aead6ffbb90
Rajiv Ramaiah <rajivramaiah@me.com>
2016-08-16 21:24:23 -0400

    added settings

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import App from './components/app';
 import Main from './containers/main';
 import CreateSnap from './containers/create-snap.js';
 import ShowSnap from './containers/show-snap.js';
+import Settings from './containers/settings.js';
 
 export default(
   <Route path="/" component={App}>
     <IndexRoute component={Main} />
     <Route path="snaps/new" component={CreateSnap} />
     <Route path="snaps/:id" component={ShowSnap} />
+    <Route path="/settings" component={Settings} />
   </Route>
 );
