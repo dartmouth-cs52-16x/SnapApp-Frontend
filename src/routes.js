a166617f2de2611a6e5dd20e082509939a602fe1
Rajiv Ramaiah <rajivramaiah@me.com>
2016-08-17 10:57:25 -0400

    implemented image uploading

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Main from './containers/main';
 import CreateSnap from './containers/create-snap.js';
 import ShowSnap from './containers/show-snap.js';
 import Settings from './containers/settings.js';
+import Profile from './containers/profile.js';
 
 export default(
   <Route path="/" component={App}>
@@ -12,5 +13,6 @@ export default(
     <Route path="snaps/new" component={CreateSnap} />
     <Route path="snaps/:id" component={ShowSnap} />
     <Route path="/settings" component={Settings} />
+    <Route path="/profile" component={Profile} />
   </Route>
 );
